ce800afd3738a304e50837edfb682105f4812904
LIKITH G <118548660+Likithgovind@users.noreply.github.com>
2025-04-08 22:06:16 +0530

    Create Minimum Number of Operations to Make Elements in Array Distinct.py

---
+++ Minimum Number of Operations to Make Elements in Array Distinct.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def minimumOperations(self, nums):
+        operations = 0
+        while len(nums) > len(set(nums)): 
+            nums = nums[3:] 
+            operations += 1
+        return operations
