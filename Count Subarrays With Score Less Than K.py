cac1041efccec3e50cf99700cbf8284e8873b8ae
LIKITH G <118548660+Likithgovind@users.noreply.github.com>
2025-04-28 21:12:42 +0530

    Create Count Subarrays With Score Less Than K.py

---
+++ Count Subarrays With Score Less Than K.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def countSubarrays(self, nums, k):
+        n = len(nums)
+        res, total = 0, 0
+        i = 0
+        for j in range(n):
+            total += nums[j]
+            while i <= j and total * (j - i + 1) >= k:
+                total -= nums[i]
+                i += 1
+            res += j - i + 1
+        return res
