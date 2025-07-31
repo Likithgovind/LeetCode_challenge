ff60c607cfdfe0e2d5ca32bc9c1b9fea8ee6c5ef
LIKITH G <118548660+Likithgovind@users.noreply.github.com>
2025-07-31 13:01:51 +0530

    Create Bitwise ORs of Subarrays.py

---
+++ Bitwise ORs of Subarrays.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def subarrayBitwiseORs(self, arr):
+        result = set()
+        curr = set()
+        for num in arr:
+            curr = {num | x for x in curr} | {num}
+            result |= curr
+        return len(result)
