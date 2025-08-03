582833d5529d8b99f84dd1db3153c29146aaa325
LIKITH G <118548660+Likithgovind@users.noreply.github.com>
2025-08-03 18:54:49 +0530

    Create Remove Duplicates from Sorted Array II.py

---
+++ Remove Duplicates from Sorted Array II.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        i=0
+        while i<len(nums)-2:
+            if nums[i]==nums[i+2]:
+                nums.pop(i)
+            else:
+                i+=1
+        return len(nums)
