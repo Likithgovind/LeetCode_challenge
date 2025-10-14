b883b371c1c3fabdf9ac5462c8233941b58e3847
LIKITH G <118548660+Likithgovind@users.noreply.github.com>
2025-10-14 23:31:25 +0530

    Create Adjacent Increasing Subarrays Detection I.py

---
+++ Adjacent Increasing Subarrays Detection I.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def hasIncreasingSubarrays(self, nums, k):
+        prev, cur, k2 = 0, 1, k*2
+        for i in range(1, len(nums)):
+            if nums[i-1] < nums[i]:
+                cur += 1
+            else:
+                prev, cur = cur, 1
+            if (cur >= k and prev >= k) or cur >= k2:
+                return True
+        return False
